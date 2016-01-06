866a76ecf314869b5d5c9b6b3f3b7af660f4dadc
Michael Schlottke-Lakemper <michael@sloede.com>
2016-01-06 13:11:39 +0100

    Finish command line parsing algorithm

--- src/sss-mon.cpp
+++ src/sss-mon.cpp
@@ -36,7 +36,7 @@ void usage(std::ostream& os = std::cerr) {
      << "                        interface in '/proc/net/dev' (default: "
      << DEFAULT_NETWORK_INTERFACE << ").\n"
      << "  -p, --period PERIOD   Set sampling period (in seconds). Must be a\n"
-     << "                        non-negative integer value (default: "
+     << "                        positive integer value (default: "
      << DEFAULT_PERIOD << ").\n"
      << "\n"
      << "For each sample, a space-seperated list of the following fields is\n"
@@ -105,7 +105,7 @@ int main(int argc, char* argv[]) {
 
       case 'h':
         {
-          usage();
+          usage(cout);
           exit(0);
         }
 
@@ -117,17 +117,23 @@ int main(int argc, char* argv[]) {
 
       case 'p':
         {
-          stringstream ss(optarg);
-          cout << "jo" << endl;
-          ss >> args.period;
-          cout << args.period << endl;
-          cout << "man" << endl;
+          istringstream arg(optarg);
+          arg >> args.period;
+          if (arg.fail() || arg.get() != istringstream::traits_type::eof()) {
+            cerr << "error: argument to '-p|--period' (" << optarg
+                 << ") is not an integer" << endl;
+            exit(2);
+          } else if (args.period < 1) {
+            cerr << "error: argument to '-p|--period' (" << optarg
+                 << ") is less than one" << endl;
+            exit(2);
+          }
+          break;
         }
 
       case '?':
         {
           usage();
-          cout << "?" << endl;
           exit(2);
           break;
         }
@@ -140,94 +146,6 @@ int main(int argc, char* argv[]) {
         }
     }
   }
-  cout << "Network interface: " << args.network_interface << endl;
-  cout << "Period:            " << args.period << endl;
-  return 0;
-
-  int c, verbose_flag;
-
-  while (1)
-    {
-      static struct option long_options[] =
-        {
-          /* These options set a flag. */
-          {"verbose", no_argument,       &verbose_flag, 1},
-          {"brief",   no_argument,       &verbose_flag, 0},
-          /* These options don’t set a flag.
-             We distinguish them by their indices. */
-          {"add",     no_argument,       0, 'a'},
-          {"append",  no_argument,       0, 'b'},
-          {"delete",  required_argument, 0, 'd'},
-          {"create",  required_argument, 0, 'c'},
-          {"file",    required_argument, 0, 'f'},
-          {0, 0, 0, 0}
-        };
-      /* getopt_long stores the option index here. */
-      int option_index = 0;
-
-      c = getopt_long (argc, argv, "abc:d:f:",
-                       long_options, &option_index);
-
-      /* Detect the end of the options. */
-      if (c == -1)
-        break;
-
-      switch (c)
-        {
-        case 0:
-          /* If this option set a flag, do nothing else now. */
-          if (long_options[option_index].flag != 0)
-            break;
-          printf ("option %s", long_options[option_index].name);
-          if (optarg)
-            printf (" with arg %s", optarg);
-          printf ("\n");
-          break;
-
-        case 'a':
-          puts ("option -a\n");
-          break;
-
-        case 'b':
-          puts ("option -b\n");
-          break;
-
-        case 'c':
-          printf ("option -c with value `%s'\n", optarg);
-          break;
-
-        case 'd':
-          printf ("option -d with value `%s'\n", optarg);
-          break;
-
-        case 'f':
-          printf ("option -f with value `%s'\n", optarg);
-          break;
-
-        case '?':
-          /* getopt_long already printed an error message. */
-          break;
-
-        default:
-          abort ();
-        }
-    }
-
-  /* Instead of reporting ‘--verbose’
-     and ‘--brief’ as they are encountered,
-     we report the final status resulting from them. */
-  if (verbose_flag)
-    puts ("verbose flag is set");
-
-  /* Print any remaining command line arguments (not options). */
-  if (optind < argc)
-    {
-      printf ("non-option ARGV-elements: ");
-      while (optind < argc)
-        printf ("%s ", argv[optind++]);
-      putchar ('\n');
-    }
-  return 0;
 
   while (true) {
     // Current point in time as reference
